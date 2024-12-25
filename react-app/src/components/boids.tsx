0fb20dd34335a03d3c1d036223140507d46cf5b0
John Akinmolayan <jakinmo1@umbc.edu>
2024-12-25 12:49:59 -0500

    have not completed boids, come back to it later

--- react-app/src/components/boids.tsx
+++ react-app/src/components/boids.tsx
@@ -1,29 +1,28 @@
-// DoublePendulumComponent.tsx
 
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
 
 // Boid
-  class boid {
+  // class boid {
 
-     pos_x:number;
-     pos_y: number;
+  //    pos_x:number;
+  //    pos_y: number;
 
-    velocity:number;
+  //   velocity:number;
 
-    acceletation:number;
+  //   acceletation:number;
 
-    constructor(pos_x = 0, pos_y = 0,velocity = 0, acceletation=0){
+  //   constructor(pos_x = 0, pos_y = 0,velocity = 0, acceletation=0){
 
-      this.pos_x = pos_x;
-      this.pos_y = pos_y;
+  //     this.pos_x = pos_x;
+  //     this.pos_y = pos_y;
 
-      this.velocity = velocity;
+  //     this.velocity = velocity;
 
-      this.acceletation = acceletation;
+  //     this.acceletation = acceletation;
 
-    }
+  //   }
     
      
 
-  }
+  // }
 
